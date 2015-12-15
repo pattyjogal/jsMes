a5a2412f8926b15cbcb4cb75ca6269f582dba759
Patrick Gallagher <patrickj@cpgallagher.com>
2015-12-15 16:31:44 -0600

    Working calculator

--- Definitons.js
+++ Definitons.js
@@ -1,20 +1,12 @@
 //Here are some predefined unit variables:
 var unitDefs = [
-{ meter : new Unit("meter", "m", 1)},
-
-{ gram : new Unit("gram", "g", 1)},
-
-{ second : new Unit("second", "s", 1)},
-
-{ ampere : new Unit("amp", "A", 1)},
-
-{ kelivn : new Unit("kelivn", "K", 1)},
-
-{ mole : new Unit("mole", "mol", 1)},
-
-{ candela : new Unit("candela", "cd", 1)},
-
-{ hertz : new Unit("hertz", "Hz", 1)},
-
-{ newton : new Unit("newton", "N", 1)},
+ new Unit("meter", "m", 1),
+ new Unit("gram", "g", 1),
+ new Unit("second", "s", 1),
+ new Unit("amp", "A", 1),
+ new Unit("kelivn", "K", 1),
+ new Unit("mole", "mol", 1),
+ new Unit("candela", "cd", 1),
+ new Unit("hertz", "Hz", 1),
+ new Unit("newton", "N", 1),
 ];
